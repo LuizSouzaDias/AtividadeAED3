b48ef0187ca580769bd7ce4834ac9a0c913b0cf9
Luiz Dias <luizhsouzadias@gmail.com>
2023-03-13 20:15:35 -0300

    Add files via upload

---
+++ AtividadeAvaliativa3/AtividadeAvaliativa3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeAvaliativa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalComissao;
+            Vendedor vendedor = new Vendedor();  //instanciar um objeto da classe vendedor
+
+            Console.WriteLine("Digite o nome do funcionário, seu salário fixo e seu total de vendas");
+            vendedor.setNome(Console.ReadLine());
+            vendedor.setSalario(double.Parse(Console.ReadLine()));
+            vendedor.setTotalVendas(double.Parse(Console.ReadLine()));
+
+            totalComissao = vendedor.CalcularSalarioTotal(vendedor.getTotalVendas(),vendedor.getSalario());
+
+            Console.WriteLine("O funcionário " + vendedor.getNome() + " irá receber: " + totalComissao.ToString("F"));
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ AtividadeAvaliativa3/AtividadeAvaliativa3/Vendedor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeAvaliativa3
+{
+    internal class Vendedor
+    {
+        private string nome;
+        private double salario;
+        private double totalVendas;
+
+        public Vendedor() { }
+
+        public Vendedor(string nome, double salario, double totalVendas)
+        {
+            this.nome = nome;
+            this.salario = salario;
+            this.totalVendas = totalVendas;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setSalario(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return this.salario;
+        }
+
+        public void setTotalVendas(double totalVendas)
+        {
+            this.totalVendas = totalVendas;
+        }
+
+        public double getTotalVendas()
+        {
+            return this.totalVendas;
+        }
+
+        public double CalcularSalarioTotal(double totalVendas, double salario)
+        {
+            double totalComissao;
+            totalComissao = salario + (totalVendas * 0.15);
+            return totalComissao;
+        }
+    }
+}
